d1d181381cbbd196292d3aaa007d81653b3c0c34
Ivan <69751600+i-loading@users.noreply.github.com>
2020-11-04 16:58:52 +0200

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
 $(document).ready(function () {
-  
+  $(".index-header .menu li a").click(function (e) { 
+    $(".index-header .menu li a").removeClass("active"); 
+    $(this).addClass("active");
+  });
 });
